c981c6c967cb59cdb2c53bb18c78ed27de79b871
wangding0421 <wangding0421@gmail.com>
2015-05-02 12:33:23 +0800

    delete

--- .emacs.d/elpa/rvm-1.3.0/rvm-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "rvm" "1.3.0" "Emacs integration for rvm" 'nil :url "http://www.emacswiki.org/emacs/RvmEl" :keywords '("ruby" "rvm"))
--- .emacs.d/elpa/simpleclip-1.0.0/simpleclip-pkg.el
+++
@@ -1,1 +0,0 @@
-(define-package "simpleclip" "1.0.0" "Simplified access to the system clipboard" 'nil :url "http://github.com/rolandwalker/simpleclip" :keywords '("convenience"))
